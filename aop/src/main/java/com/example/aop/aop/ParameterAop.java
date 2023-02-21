da850a6ae5bd0f84a1de572a0733d92a875374e2
i960107 <i960107@naver.com>
2023-02-21 11:18:31 +0900

    Rename Proejcts And Add Ignore Files

--- aop/src/main/java/com/example/aop/aop/ParameterAop.java
+++ aop/src/main/java/com/example/aop/aop/ParameterAop.java
@@ -1,10 +1,12 @@
 package com.example.aop.aop;
 
+import java.lang.reflect.Method;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,6 +19,9 @@ public class ParameterAop {
     @Before("pointCut()")
     public void before(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName());
         for (Object obj : args) {
             System.out.println("type :  " + obj.getClass().getSimpleName());
             System.out.println("value " + obj);
